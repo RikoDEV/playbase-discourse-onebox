2fbed0b4353196c169d290829d4c78c962f99526
RikoDEV <18230443+RikoDEV@users.noreply.github.com>
2023-04-25 15:36:17 +0200

    Update domain

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![msedge_2fvqyezkPt](https://user-images.githubusercontent.com/18230443/212159266-9efc07ab-d3d0-4f35-9957-d40f4ff408c2.png)
 
 # poszukajse-discourse-onebox
-Adds support for embedding poszukaj.se servers within Discourse.
+Adds support for embedding playbase.pro servers within Discourse.
 
 Created for https://anyforum.pl/
--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
-# name: Discourse poszukaj.se onebox
-# about: Adds support for embedding poszukaj.se servers within Discourse.
-# version: 1.0.3
+# name: Discourse playbase.pro onebox
+# about: Adds support for embedding playbase.pro servers within Discourse.
+# version: 1.0.4
 # authors: riko.dev
 # url: https://github.com/rikodev/poszukajse-discourse-onebox
 
@@ -8,10 +8,10 @@ class Onebox::Engine::PoszukajSEOnebox
 	include Onebox::Engine
 	include Onebox::Engine::StandardEmbed
 
-	REGEX = /^https?:\/\/poszukaj.se\/(?:\w+\/?)(\w*.+)\/(\d+)/
+	REGEX = /^https?:\/\/playbase.pro\/(?:\w+\/?)(\w*.+)\/(\d+)/
 	matches_regexp REGEX
 
-	requires_iframe_origins("https://img.poszukaj.se")
+	requires_iframe_origins("https://img.playbase.pro")
 	always_https
 
 	def id
@@ -25,18 +25,18 @@ class Onebox::Engine::PoszukajSEOnebox
 		<<-HTML
 			<aside class="onebox allowlistedgeneric" data-onebox-src="#{@url}">
 				<header class="source">
-					<img src="https://poszukaj.se/storage/branding/logo.png" class="site-icon" style="width: auto;height: 18px;">
+					<img src="https://playbase.pro/storage/branding/logo.png" class="site-icon" style="width: auto;height: 18px;">
 				</header>
 			
 				<article class="onebox-body">
-					<img src="https://poszukaj.se/storage/servers/#{id}.png" class="thumbnail size-resolved onebox-avatar">
+					<img src="https://playbase.pro/storage/servers/#{id}.png" class="thumbnail size-resolved onebox-avatar">
 					<h3>
 						<a href="#{@url}" target="_blank" rel="nofollow ugc noopener" tabindex="-1">#{og.title}</a>
 					</h3>
 				
 					<p>#{og.description}</p>
 
-					<img src="https://img.poszukaj.se/banners/server/#{id}/small" style="padding-bottom: 0px;">
+					<img src="https://img.playbase.pro/banners/server/#{id}/small" style="padding-bottom: 0px;">
 				
 				</article>
 			
